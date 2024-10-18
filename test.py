e123c1ec55b85e96f1122f2573b30a490b742831
HowardWei123 <howieweiinf@gmail.com>
2024-10-17 21:54:11 -0700

    cleaned up code for readable output

--- test.py
+++ test.py
@@ -1,21 +1,6 @@
-from alpaca.data.historical import CryptoHistoricalDataClient
-
-# No keys required for crypto data
-client = CryptoHistoricalDataClient()
-
-from alpaca.data.requests import CryptoBarsRequest
 from alpaca.data.timeframe import TimeFrame
-
-# Creating request object
-request_params = CryptoBarsRequest(
-  symbol_or_symbols=["BTC/USD"],
-  timeframe=TimeFrame.Day,
-  start="2024-10-03",
-  end="2024-10-11"
-)
-
-# above code does not need API key
 from alpaca.data import StockHistoricalDataClient, StockTradesRequest
+from alpaca.data.live import StockDataStream
 import os
 from dotenv import load_dotenv
 
@@ -25,30 +10,38 @@ load_dotenv()
 alpaca_api_key = os.getenv("APCA-API-KEY-ID")
 alpaca_secret_key = os.getenv("APCA-API-SECRET-KEY")
 
-stock_hist_client = StockHistoricalDataClient(alpaca_api_key, alpaca_secret_key)
+target_symbols = 'NVDA'
 
-request_params = StockTradesRequest(
-  symbol_or_symbols=["AAPL"],
-  timeframe=TimeFrame.Day,
-  start="2024-10-15",
-  end="2024-10-15",
-  limit=100
-)
+def stock_historical():
+  stock_hist_client = StockHistoricalDataClient(alpaca_api_key, alpaca_secret_key)
 
-trades = stock_hist_client.get_stock_trades(request_params=request_params)
+  request_params = StockTradesRequest(
+    symbol_or_symbols=target_symbols,
+    timeframe=TimeFrame.Day,
+    start="2024-10-17",
+    end="2024-10-18",
+    limit=10
+  )
 
-print(trades)
+  trades = stock_hist_client.get_stock_trades(request_params=request_params)
 
-from alpaca.data.live import StockDataStream
+  for trade in trades.data[target_symbols]:
+     print(trade)
+     print()
 
 
-stream = StockDataStream(alpaca_api_key, alpaca_secret_key)
+def stock_live():
+  stream = StockDataStream(alpaca_api_key, alpaca_secret_key)
 
-# async handler
-async def quote_data_handler(data):
-    # quote data will arrive here
-    print(data)
+  # async handler
+  async def quote_data_handler(data):
+      # quote data will arrive here
+      print(data)
+      print()
 
-stream.subscribe_quotes(quote_data_handler, "AAPL")
+  stream.subscribe_quotes(quote_data_handler, target_symbols)
 
-stream.run()
+  stream.run()
+
+stock_historical()
+stock_live()
